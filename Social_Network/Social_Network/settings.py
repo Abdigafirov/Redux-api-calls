c181b758d6a3bfe6971f9d623b07a8e4037b14eb
Abdigafirov <biloldina87@gmail.com>
2023-08-06 14:37:09 -0700

    first commit

--- Social_Network/Social_Network/settings.py
+++ Social_Network/Social_Network/settings.py
@@ -11,21 +11,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(os.path.join(BASE_DIR, '../.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_92^%l3m51kb59((7#^!jdjt!gt0vznn^n___*gl_xu)rj2@!z'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onrender.com', '127.0.0.1', 'render.com']
 
 
 # Application definition
@@ -81,16 +87,11 @@ WSGI_APPLICATION = 'Social_Network.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'exam',
-        'USER': 'postgres',
-        'PASSWORD': 'Biloldin2006',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
+    'default': dj_database_url.config(
+        default=env('DATABASE')
+    )
 }
 
 
